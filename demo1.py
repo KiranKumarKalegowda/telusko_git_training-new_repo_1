3202073e0531948df11d7aae0b9ac6fc784dc54f
KiranKumarKalegowda <104920866+KiranKumarKalegowda@users.noreply.github.com>
2022-09-14 08:28:40 +0530

    added new functionality 002
    added a new add function to my demo1.py file

--- demo1.py
+++ demo1.py
@@ -1,3 +1,9 @@
 print("hello git_repo")
 print("creating new git repo for pycharm workout purpose")
 print("new line to demo1 file")
+
+## simple add function
+
+def add(a,b):
+  print("adding the two numbers")
+  return a+b
